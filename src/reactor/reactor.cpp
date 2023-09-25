5434bbc201520f0b7dfbba6f954c8e1f3639d636
Yaozn <zinan@outlook.com>
2023-09-25 22:37:52 +0800

    两种模式基本上可以稳定运行

--- src/reactor/reactor.cpp
+++ src/reactor/reactor.cpp
@@ -13,6 +13,7 @@ CUB_NS_BEGIN
 struct LocalSocket {
     LocalSocket( zmq::context_t& ctx_, zmq::socket_type type_ )
         : sock( ctx_, type_ ) {
+        LOG_INFO( "local sock created, ctx=0x%lx, sock=0x%lx", ( intptr_t )&ctx_, ( intptr_t )&sock );
     }
 
     zmq::socket_t* operator->() {
@@ -22,12 +23,13 @@ struct LocalSocket {
     void connect( const char* addr_ ) {
         if ( connected ) return;
 
-        // todo  connect 是有时间的,如果立刻ub消息,可能会导致第一条消息丢失, 可以等welcome msg之后再发送
+        // warn:  connect 是有时间的,如果立刻ub消息,可能会导致第一条消息丢失,welcome msg 只给sub用的，这里简单的等待2ms
         // https://github.com/zeromq/libzmq/issues/2267--zmq的welcome msg可能就是因为这个bug添加的
         sock.connect( addr_ );
         connected = true;
+
         // todo verify ..
-        std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
+        std::this_thread::sleep_for( std::chrono::milliseconds( 2 ) );
     }
 
     bool connected = false;
@@ -57,11 +59,11 @@ int Reactor::pub( const void* data_, size_t length_ ) {
 
     auto& sock = distribute( h->id );
 
-    zmq::const_buffer buff{ data_, length_ };
-    // fprintf( stderr, "before send" );
-    zmq::send_result_t rc = sock.send( buff, zmq::send_flags::none );
-
-    fprintf( stderr, "send ok sock = 0x%lx ,rc=%lu\n", ( int64_t )&sock, rc.has_value() ? rc.value() : 0 );
+    zmq::const_buffer  buff{ data_, length_ };
+    zmq::send_result_t rc = sock.send( buff, zmq::send_flags::dontwait );
+    if ( !rc.has_value() ) {
+        LOG_INFO( "send fail. sock = 0x%lx", &sock );
+    }
 
     return rc.has_value() ? 0 : -1;
 }
@@ -76,7 +78,7 @@ Reactor::Reactor() {
 }
 
 void Reactor::init_svc() {
-    fprintf( stderr, "##init svc\n" );
+    LOG_INFO( "##init svc" );
     _data.init( REACTOR_DATA );
     _trade.init( REACTOR_TRADE );
 }
@@ -124,6 +126,7 @@ void Reactor::Svc::init( const std::string& endpoint_ ) {
     assert( context );
     assert( chan );
 
+    LOG_INFO( "context=0x%lx ,sock=0x%lx, endpoint=%s", ( intptr_t )&context, ( intptr_t )&chan, endpoint_.c_str() );
     chan.bind( endpoint.c_str() );
 }
 
@@ -134,7 +137,6 @@ void Reactor::filter_from_id( FilterToken& filter_, const msg::mid_t& id_ ) {
     while ( uid ) {
         filter_[ index++ ] = uid & 0xFF;
         LOG_TRACE( "%02X ", filter_[ index - 1 ] );
-        fprintf( stderr, "%02X ", filter_[ index - 1 ] );
         uid >>= 8;
     }
 }
@@ -143,6 +145,7 @@ int Reactor::sub( const MsgIdSet& msg_set_, MsgHandler h_ ) {
     LOG_INFO( "prepare to recv messages." );
     static constexpr int kMaxMessgageLength = 1204;
 
+    // warn capture h_ by value
     auto loop = [ = ]( zmq::socket_t& sock_ ) {
         std::unique_ptr<char[]> m = std::make_unique<char[]>( kMaxMessgageLength );
 
@@ -150,44 +153,41 @@ int Reactor::sub( const MsgIdSet& msg_set_, MsgHandler h_ ) {
 
         msg::Header* header = reinterpret_cast<msg::Header*>( m.get() );
         while ( 1 ) {
-            // fprintf( stderr, "recv sock=0x%lx\n", ( uint64_t )&sock_ );
             auto rc = sock_.recv( rbuff, zmq::recv_flags::none );
             if ( !rc.has_value() ) {
-                printf( "bad receive\n" );
                 LOG_INFO( "recv error" );
                 header->id = msg::mid_t::exception;
             }
             else {
-                // fprintf( stderr, "good receive %lu\n", rc.value().size );
-                LOG_INFO( "got message" );
             }
 
             h_( *header );
-        }  // while(1)
+        }
     };
 
     std::thread( [ = ]() {
         zmq::socket_t chan;
 
-        fprintf( stderr, "create sub thread\n" );
+        LOG_INFO( "create sub thread" );
 
         if ( msg_set_.size() == 1 && *msg_set_.begin() == msg::mid_t::svc_data ) {
-            fprintf( stderr, "svc data sub\n" );
+            LOG_INFO( "svc data sub" );
 
+            // note 从源代码可以看到，inproc属于single connection，后续的连接会被忽略
             zmq::socket_t d = zmq::socket_t( _data.context, zmq::socket_type::pair );
             d.connect( REACTOR_DATA );
             chan.swap( d );
         }
         else if ( msg_set_.size() == 1 && *msg_set_.begin() == msg::mid_t::svc_order ) {
-
-            fprintf( stderr, "create svc-trade sub thread\n" );
+            LOG_INFO( "create svc-trade sub " );
             zmq::socket_t t = zmq::socket_t( _trade.context, zmq::socket_type::pair );
             t.connect( REACTOR_TRADE );
             chan.swap( t );
         }
         else {
-            fprintf( stderr, "create comm sub thread\n" );
             zmq::socket_t sub( _center_ctx, ZMQ_SUB );
+
+            LOG_INFO( "create comm sub thread: context=0x%lx, sock=0x%lx", ( intptr_t )&_center_ctx, ( intptr_t )&sub );
             if ( msg_set_.empty() ) {
                 sub.set( zmq::sockopt::subscribe, "" );
             }
@@ -208,9 +208,9 @@ int Reactor::sub( const MsgIdSet& msg_set_, MsgHandler h_ ) {
             chan.swap( sub );
         }
 
-        fprintf( stderr, "into loop\n" );
+        LOG_INFO( "into loop" );
         loop( chan );
-        fprintf( stderr, "loop finished\n" );
+        LOG_INFO( "loop finished" );
         chan.close();
     } ).detach();
     return 0;
--- src/reactor/test.pub.cpp
+++ src/reactor/test.pub.cpp
@@ -7,50 +7,26 @@
 int main() {
     LOG_INIT_KEEP( "log_reactor.log", -1 );
 
-
-//如果下面两个sub都开启,后面的pub无论那种消息都会被第二个sub拦截
-//如果只开第一个sub,但是却pub 了两种消息,则会卡在第二个pub上  
-
-
-#if 1
     REACTOR.sub( { cub::msg::mid_t::data_tick }, []( const cub::msg::Header& h ) {
         printf( "@@sub,code=%u, %d\n", ( unsigned )h.id, cub::msg::frame_cast<CUB_NS::msg::DataTickFrame>( h ).body.debug );
     } );
-#endif
 
-  REACTOR.sub( { cub::msg::mid_t::svc_data }, []( const cub::msg::Header& h ) {
-        // printf( "svc+data code=%u\n", ( unsigned )h.id );
-        printf( "###sub,code=%u, %d\n", ( unsigned )h.id, cub::msg::frame_cast<CUB_NS::msg::DataTickFrame>( h ).body.debug );
+    REACTOR.sub( { cub::msg::mid_t::data_tick }, []( const cub::msg::Header& h ) {
+        printf( "&&sub,code=%u, %d\n", ( unsigned )h.id, cub::msg::frame_cast<CUB_NS::msg::DataTickFrame>( h ).body.debug );
     } );
-
-#if 0
-    std::thread( [ & ]() {
-        int seq = 10000;
-        for ( ;; ) {
-            CUB_NS::msg::DataTickFrame d;
-            d.body.debug = seq++;
-
-            fprintf( stderr, "pub---%d\n", d.body.debug );
-            REACTOR.pub( d );
-
-            d.h.id = cub::msg::mid_t::svc_data;
-            REACTOR.pub( d );
-            ::sleep( 2 );
-        }
-    } ).detach();
-#endif
 
     std::thread( [ & ]() {
         int seq = 0;
         for ( ;; ) {
             CUB_NS::msg::DataTickFrame d;
             d.h.id       = cub::msg::mid_t::data_tick;
-            d.body.debug = seq;
+            d.body.debug = ++seq;
 
-            if ( ++seq >= 15 ) {
-                fprintf( stderr, "-------\n" );
-                ::sleep( 1 );
-                continue;
+            if ( seq == 3 ) {
+                REACTOR.sub( { cub::msg::mid_t::svc_data }, []( const cub::msg::Header& h ) {
+                    // printf( "svc+data code=%u\n", ( unsigned )h.id );
+                    printf( "###sub,code=%u, %d\n", ( unsigned )h.id, cub::msg::frame_cast<CUB_NS::msg::DataTickFrame>( h ).body.debug );
+                } );
             }
 
             fprintf( stderr, "pub---%d\n", d.body.debug );
@@ -60,7 +36,7 @@ int main() {
             d.h.id = cub::msg::mid_t::svc_data;
 
             // todo 如果inproc数据没有订阅者,这里会在send函数中卡住
-             REACTOR.pub( d );
+            REACTOR.pub( d );
             ::sleep( 1 );
         }
     } ).join();
