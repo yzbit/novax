1f69bda1f1d2677ae49251ba1d70b2a2fef1f547
Yaozn <zinan@outlook.com>
2024-05-06 15:13:20 +0800

    chore: license

--- src/core/canlendar.h
+++ src/core/canlendar.h
@@ -1,3 +1,31 @@
+/************************************************************************************
+MIT License
+
+Copyright (c) 2024 [YaoZinan zinan@outlook.com nvx-quant.com]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*@init: Yaozn
+*@contributors:Yaozn qianq
+*@update: 2024
+**********************************************************************************/
+
 #ifndef BB813C61_80B5_4C65_966A_8F423BC7ECEA
 #define BB813C61_80B5_4C65_966A_8F423BC7ECEA
 #include "definitions.h"
